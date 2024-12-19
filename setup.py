c4c668b78504c0d7c23109aa3e7f54906cf85061
nesquikcode <157715597+nesquikcode@users.noreply.github.com>
2024-12-19 18:10:23 +0300

    some update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tgcodes',
-    version='0.1.01',
+    version='0.1.1',
     description='API Client for https://gateway.telegram.org/.',
     author='nesquikcode',
     author_email='nesquik@nishine.ru',
